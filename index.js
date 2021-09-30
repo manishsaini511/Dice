b5809cec3f8e583e695b7fec48f18b9c64ce2770
manishsaini511 <54799227+manishsaini511@users.noreply.github.com>
2021-09-30 11:00:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 var img2 = document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
 
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player1 Won!"
+}  else if(randomNumber2 > randomNumber1) {
+    
+document.querySelector("h1").innerHTML = "Player2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
