92b5b0cbd36b52e7379e35be3324924167cd3da4
manishsaini511 <54799227+manishsaini511@users.noreply.github.com>
2021-09-30 10:58:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var randomNumber1 = Math.floor(Math.random()*6+1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var img1 = document.querySelectorAll("img") [0];
+
+img1.setAttribute("src", randomImageSource);
 
